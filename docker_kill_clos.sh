8d1b6e0501e645750512d783851619b992b13c14
Pavel May <pavelmay@snaproute.com>
2017-04-26 16:13:14 -0400

    More complex example with better niceties * Make a more complex example (3-layer CLOS with: 2 x cores, 4 x spines, and 8 x leaves) * Keep track of the container IDs of what was started in a file * The docker_kill_clos.sh now reads the file to do the cleanup * Cleanup now cleans up the IP namespace, too * Removing --privileged and limiting the capabilities being requested by the docker invocation

---
+++ docker_kill_clos.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+if [ $# -ne 1 ]; then
+  echo "Expected one parameters - filename containing the containers created by the companion script"
+  exit 1
+fi # if [ $# -ne 1 ]
+
+
+container_record=$1
+if [ -r "$container_record" ]; then
+  IFS=","
+  cat $container_record | while read cid name namespace; do
+    echo "Container $name (id: $cid): "
+    echo -en "\tDocker stop: "
+    sudo docker stop $cid
+    echo -en "\tDocker rm: "
+    sudo docker rm $cid
+    echo -e "\tip namespace ($namespace) cleanup."
+    sudo ip netns delete $namespace
+  done # while read cid name namespace
+  unset IFS
+else
+  echo "ERROR: Could not read $container_record for processing."
+  exit 1
+fi
