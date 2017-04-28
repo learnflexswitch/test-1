48d9de89c56e6bcefbfac3344c77bb8433915e31
Pavel May <pavelmay@snaproute.com>
2017-04-27 21:02:38 -0400

    Initial commit

---
+++ clos/initial_config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# spine4,eth1,core2,eth1
+# spine4,eth2,core1,eth1
+# spine3,eth1,core2,eth2
+#b7e89891af16c64599e05a31d5e0161ed0cfbe71df34bf316993e9ddb1eea926,core1,21629,172.17.0.2
+#2f772b7d8b9e91396334ec6cd32f6e677928ba24b24f09109c9e416fd3c7b33d,core2,21796,172.17.0.3
+#5491649ff892284be536ebf7fa6cc016839b2a81bc2e4b0b1e5de8bbf7d54b59,spine1,21971,172.17.0.4
+
+IFS=","
+cat containers.lst | while read cid name ns ip; do
+  echo "fluffy.py --host $ip BGPGlobal config PATCH --json_blob '{\"ASNum\":\"$ns\",\"RouterID\":\"$name:$ns\"}'"
+done
+
+sort -t "," -k1 netlinks | while read src srcnic dst dstnic; do 
+  grep "$src" containers.lst | while read cid name ns ip; do
+    echo "fluffy.py --host $ip Port config PATCH --json_blob '{\"Description\":\"$src:$srcnic to $dst:$dstnic\", \"IntfRef\":\"$srcnic\"}'"
+    #echo "fluffy.py --host $name Port config PATCH --json_blob '{\"Description\":\"$src:$srcnic to $dst:$dstnic\", \"IntfRef\":\"$srcnic\"}'"
+  done
+done
+sort -t "," -k3 netlinks | while read src srcnic dst dstnic; do 
+  grep "$dst" containers.lst | while read cid name ns ip; do
+    echo "fluffy.py --host $ip Port config PATCH --json_blob '{\"Description\":\"$dst:$dstnic to $src:$srcnic\", \"IntfRef\":\"$dstnic\"}'"
+    #echo "fluffy.py --host $name Port config PATCH --json_blob '{\"Description\":\"$dst:$dstnic to $src:$srcnic\", \"IntfRef\":\"$dstnic\"}'"
+  done
+done
+
+unset IFS 
